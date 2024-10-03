96f152c622932364b6e7da919304706e23063965
Mr.Shankar Aryal <shankararyal737@gmail.com>
2024-10-03 20:48:43 +0545

    Update nav.js

--- Asssets/js/nav.js
+++ Asssets/js/nav.js
@@ -1,25 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('DOM fully loaded and parsed');
-    
     const hamburger = document.querySelector("#hamburger");
     const navMenu = document.querySelector("#nav-menu");
     
-    console.log('Hamburger element:', hamburger);
-    console.log('Nav menu element:', navMenu);
-
     if (hamburger && navMenu) {
         hamburger.addEventListener("click", function() {
-            console.log('Hamburger clicked');
             hamburger.classList.toggle("active");
             navMenu.classList.toggle("active");
         });
+
+        document.querySelectorAll(".nav-links a").forEach(n => n.addEventListener("click", function() {
+            hamburger.classList.remove("active");
+            navMenu.classList.remove("active");
+        }));
     } else {
         console.error('Hamburger or nav menu element not found');
     }
-
-    document.querySelectorAll(".nav-links a").forEach(n => n.addEventListener("click", function() {
-        console.log('Nav link clicked');
-        hamburger.classList.remove("active");
-        navMenu.classList.remove("active");
-    }));
 });
